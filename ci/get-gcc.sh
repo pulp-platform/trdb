ba2930ebf32f60eb0d43b781575a8b8deade8bef
bluew <bluewww@users.noreply.github.com>
2019-03-27 13:52:29 +0100

    Change gcc checkout behaviour

--- ci/get-gcc.sh
+++ ci/get-gcc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-
+ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 VERSION="afcc8bc655d30cf6af054ac1d3f5f89d0627aa79"
-
+#VERSION="691e4e826251c7ec59f883cab18440c87baf45e7"
 
 mkdir -p $RISCV
 
@@ -13,14 +13,15 @@ if [ -z ${NUM_JOBS} ]; then
     NUM_JOBS=1
 fi
 
-if ! [ -e $RISCV/bin/riscv32-unknown-elf-gcc ]; then
+
+if ! [ -e $RISCV/bin ]; then
     if ! [ -e $RISCV/riscv-gnu-toolchain ]; then
-	git clone -j ${NUM_JOBS} --recursive https://github.com/riscv/riscv-gnu-toolchain.git
+        git clone https://github.com/riscv/riscv-gnu-toolchain.git
     fi
 
     cd riscv-gnu-toolchain
     git checkout $VERSION
-    git submodule update --checkout
+    git submodule update --init --recursive
 
     if [[ $1 -ne "0" || -z ${1} ]]; then
       echo "Compiling RISC-V Toolchain"
