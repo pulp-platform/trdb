3a0d1b25f0f3811b26536ce2fefa2e49aba28714
bluew <bluewww@users.noreply.github.com>
2024-03-15 23:30:57 +0100

    treewide: Update copyright date

--- internal/trdb_private.h
+++ internal/trdb_private.h
@@ -1,7 +1,7 @@
 /*
  * trdb - Debugger Software for the PULP platform
  *
- * Copyright (C) 2019 Robert Balas
+ * Copyright (C) 2024 Robert Balas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
